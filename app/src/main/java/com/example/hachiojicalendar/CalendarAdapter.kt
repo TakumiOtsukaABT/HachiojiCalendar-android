3a0a0de86b4003c80ebdf3c34aa7b216cf3f1959
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-07-25 23:39:48 +0900

    temporarily success

--- app/src/main/java/com/example/hachiojicalendar/CalendarAdapter.kt
+++ app/src/main/java/com/example/hachiojicalendar/CalendarAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.hachiojicalendar
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.recyclerview.widget.RecyclerView
 import java.util.*
 
@@ -10,17 +12,34 @@ class CalendarAdapter(
     private val dayOfMonth: ArrayList<String>,
     private val onItemListener: OnItemListener
 ) : RecyclerView.Adapter<CalendarViewHolder>() {
+    lateinit var context: Context
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CalendarViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val view = inflater.inflate(R.layout.calendar_cell, parent, false)
         val layoutParams = view.layoutParams
         layoutParams.height = (parent.height * 0.166666666).toInt()
+        context = parent.context
         return CalendarViewHolder(view, onItemListener)
     }
 
     override fun onBindViewHolder(holder: CalendarViewHolder, position: Int) {
         holder.dayOfMonth.text = dayOfMonth[position]
-        holder.imageView.setImageResource(R.drawable.bin)
+        if (position%2 == 0) {
+            var secondImage:ImageView = ImageView(context)
+            var thirdImage:ImageView = ImageView(context)
+
+            secondImage.setImageResource(R.drawable.gomibukuro_yellow)
+            thirdImage.setImageResource(R.drawable.gomibukuro_blue)
+            val viewParamsCenter = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            secondImage.layoutParams = viewParamsCenter
+            viewParamsCenter.weight++
+            thirdImage.layoutParams = viewParamsCenter
+            holder.imageLayout.addView(secondImage)
+            holder.imageLayout.addView(thirdImage)
+        } else {
+        }
     }
 
     override fun getItemCount(): Int {
