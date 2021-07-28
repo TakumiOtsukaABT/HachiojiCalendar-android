230e0d0d48ef77a6b333d03b074a7c1cb53d3cde
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-07-28 15:52:53 +0900

    add setting activity

--- app/src/main/java/com/example/hachiojicalendar/SettingsActivity.kt
+++ app/src/main/java/com/example/hachiojicalendar/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hachiojicalendar
 
 import android.os.Bundle
+import android.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
@@ -8,6 +9,8 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        var toolBar = findViewById(R.id.toolbar) as androidx.appcompat.widget.Toolbar
+        setSupportActionBar(toolBar)
         val actionBar = supportActionBar
         actionBar!!.title = "通知設定"
         actionBar.setDisplayHomeAsUpEnabled(true)
