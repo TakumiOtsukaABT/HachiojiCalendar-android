1c84e03bfec0ec43aa96b6db7d8ed02d94e14f2f
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-07-28 15:32:19 +0900

    add setting activity back button

--- app/src/main/java/com/example/hachiojicalendar/SettingsActivity.kt
+++ app/src/main/java/com/example/hachiojicalendar/SettingsActivity.kt
@@ -2,12 +2,14 @@ package com.example.hachiojicalendar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+        val actionBar = supportActionBar
+        actionBar!!.title = "通知設定"
+        actionBar.setDisplayHomeAsUpEnabled(true)
     }
 }
