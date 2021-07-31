5296482b310e73246eb5e03ab92a51fbc43ec75b
TakumiOtsukaABT <takumiotsuka2@yahoo.com>
2021-08-01 01:25:57 +0900

    fin

--- app/src/main/java/com/example/hachiojicalendar/OnBoardingActivity.kt
+++ app/src/main/java/com/example/hachiojicalendar/OnBoardingActivity.kt
@@ -1,9 +1,9 @@
 package com.example.hachiojicalendar
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 
 
@@ -15,5 +15,11 @@ class OnBoardingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_on_boarding)
         viewPager = findViewById(R.id.pager)
         viewPager.adapter = FragmentPagerAdapter(this)
+
+        var toolBar = findViewById(R.id.toolbar2) as androidx.appcompat.widget.Toolbar
+        setSupportActionBar(toolBar)
+        val actionBar = supportActionBar
+        actionBar!!.title = ""
+        actionBar.setDisplayHomeAsUpEnabled(false)
     }
 }
